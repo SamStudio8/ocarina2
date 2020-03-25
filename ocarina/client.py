2e3b7f5c6c5fb3fc1515c6af0c270046f0b1f0cc
Sam Nicholls <samstudio8@gmail.com>
2020-03-25 17:51:48 +0000

    enable suppression of the welcome message for people who hate whimsy and ocarinas

--- ocarina/client.py
+++ ocarina/client.py
@@ -15,29 +15,9 @@ ENDPOINTS = {
 
 def cli():
     config = util.get_config()
-    print('''
-                                 .@ 888S
-                            ;@S8888:8%tX
- :XS.                 .;8: @8t88888t8888
-X8%%S:              .8 8S888    :8.88S@:
-8;8;888:        :@; 888888@8    88%888
-%8:8%X8 t8. ;8 888.X8    88S@88888S %
- 888S@@888  8@888@S8C    888    S%S:
- .@%888%@@t.888@@8@8@8::X88C   88 %
-  ;S@X8@888S8X@@X8X88X888888%@888;
-  Xt88X@8@@XX8S8X88@88888S888S.;
-  t S888X8S8S8888X88@88%8@:@8%;
-  :888X   @888    @88S88S888X
- .88;.     %X8    8@88S8X8:
- S8@S8@   8t8888%8@:8%S@88
- 8%8;88888S@%.8S88;@8% %.
- :8.8    888    8XS:%:
- ::8St   88;   88S8;
-   ;t 8X.8;%8 8%
-''')
-    print("Hello %s." % config["MAJORA_USER"])
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--quiet", help="suppress the large welcoming ocarina", action="store_true")
     subparsers = parser.add_subparsers()
 
     biosample_parser = subparsers.add_parser("biosample", help="add a single biosample by providing fields via the CLI")
@@ -60,7 +40,31 @@ X8%%S:              .8 8S888    :8.88S@:
 
 
     args = parser.parse_args()
-    args.func(args)
+    if not args.quiet:
+        print('''
+                                 .@ 888S
+                            ;@S8888:8%tX
+ :XS.                 .;8: @8t88888t8888
+X8%%S:              .8 8S888    :8.88S@:
+8;8;888:        :@; 888888@8    88%888
+%8:8%X8 t8. ;8 888.X8    88S@88888S %
+ 888S@@888  8@888@S8C    888    S%S:
+ .@%888%@@t.888@@8@8@8::X88C   88 %
+  ;S@X8@888S8X@@X8X88X888888%@888;
+  Xt88X@8@@XX8S8X88@88888S888S.;
+  t S888X8S8S8888X88@88%8@:@8%;
+  :888X   @888    @88S88S888X
+ .88;.     %X8    8@88S8X8:
+ S8@S8@   8t8888%8@:8%S@88
+ 8%8;88888S@%.8S88;@8% %.
+ :8.8    888    8XS:%:
+ ::8St   88;   88S8;
+   ;t 8X.8;%8 8%
+''')
+        print("Hello %s." % config["MAJORA_USER"])
+
+    if hasattr(args, "func"):
+        args.func(args)
 
 def wrap_single_biosample_emit(args):
     v_args = vars(args)
